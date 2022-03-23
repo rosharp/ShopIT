baab052a28a36ddb5e86bb5f11354c61c4651e41
Ро <ro.sharp@MacBook-Pro-Ro.local>
2022-03-23 15:32:13 +1000

    Backend & CRUD & Added error handler

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const conntectDatabase = () => {
+  mongoose.connect(process.env.DB_LOCAL_URI, {
+    useUnifiedTopology: true,
+  }).then(con => {
+    console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
+  })
+}
+
+module.exports = conntectDatabase;
---
+++ backend/controllers/productController.js
@@ -0,0 +1,88 @@
+const Product = require('../models/product');
+
+// create new product => /api/v1/admin/product/new
+exports.newProduct = async (req, res, next) => {
+
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product
+  })
+}
+
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    count: products.lenght,
+    products
+  })
+}
+
+// Get single product details => /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    })
+  }
+
+  // if (!product) res.status(404).json({
+  //   success: false,
+  //   message: 'Product not found'
+  // })
+
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+
+}
+
+// Update Product => /api/admin/v1/product/:id
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    })
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  });
+
+  res.status(200).json({
+    success: true,
+    product
+  });
+}
+
+// Delete Product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    })
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: 'Product is deleted.'
+  })
+}

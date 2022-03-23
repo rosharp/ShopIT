a57bb0eded6555bf90b408e486790d0fe9d3a6ba
Ро <ro.sharp@MacBook-Pro-Ro.local>
2022-03-24 09:48:51 +1000

    Handling Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const app = require('./app');
+const connectDatapase = require('./config/database');
 
 const dotenv = require('dotenv');
+
+// Handle Uncaught Exceptions
+process.on('uncaughtException', err => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log('Shutting down server due to Uncaught Exception');
+  process.exit(1);
+});
+
+console.log(a);
 
 // Setting up config file
 dotenv.config({ path: 'backend/config/config.env' })
 
-app.listen(process.env.PORT, () => {
+// Connecting to database
+connectDatapase();
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', err => {
+  console.log(`ERROR: ${err.message}`);
+  console.log('Shutting down the server due to Unhandled Promise rejection');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 })

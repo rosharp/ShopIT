3a6c04be5073ea36cf6d7f041ff62238b33b281f
Ро <ro.sharp@MacBook-Pro-Ro.local>
2022-03-24 10:44:04 +1000

    Search products by keyword

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require('../models/product');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 
 // create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -14,10 +15,14 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   })
 })
 
-// Get all products => /api/v1/products
+// Get all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
-  const products = await Product.find();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search();
+
+  const products = await apiFeatures.query;
+
   res.status(200).json({
     success: true,
     count: products.length,

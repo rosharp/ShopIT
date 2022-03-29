99cc3a1f5fdeda34da8f21d2652dd2460da99c66
Ro Sharp <ro.moreko@gmail.com>
2022-03-29 17:34:33 +1000

    Admin - Get all Order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -48,6 +48,22 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+// Get all orders => /api/v1/admin/orders
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+  orders.forEach(order => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders
+  });
+});
+
 // Get logged in user orders => /api/v1/orders/me
 exports.myOrders = catchAsyncErrors(async (req, res, next) => {
   const order = await Order.find({ user: req.user.id });
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders, allOrders } = require('../controllers/orderController');
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
@@ -9,6 +9,8 @@ router.route('/order/new').post(isAuthenticatedUser, newOrder);
 
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/orders/me').get(isAuthenticatedUser, myOrders);
+router.route('/admin/orders').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
+
 
 
 module.exports = router;

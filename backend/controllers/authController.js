c96ab26e7d1ddf73a6b5437b2c6d8458a75638d5
Ro Sharp <ro.moreko@gmail.com>
2022-03-29 17:25:42 +1000

    Get Logged In User Orders & Get Single Order

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,4 @@
 const User = require('../models/user');
-
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController');
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
+
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+router.route('/orders/me').get(isAuthenticatedUser, myOrders);
+
 
 module.exports = router;

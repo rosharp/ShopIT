b718e7db50e4e5166fb3b255aca1a26ed43a6e15
Ro Sharp <ro.moreko@gmail.com>
2022-03-31 17:31:09 +1000

    Get All Reviews

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -125,3 +125,14 @@ exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
     success: true
   });
 })
+
+// Get product Review => /api/v1/reviews
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews
+  })
+})
+

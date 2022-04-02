50e89874c1b162013684a51829033e236a1f3710
Ro Sharp <ro.moreko@gmail.com>
2022-04-02 17:53:27 +1000

    Create Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 const container = document.getElementById('root')
 
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );

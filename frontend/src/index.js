d0416a4d8e4ac31d900da5b2624a943e95eb6614
Ro Sharp <ro.moreko@gmail.com>
2022-04-06 12:00:32 +1000

    Adding React Alert

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,23 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { positions, transitions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE
+}
+
 const container = document.getElementById('root')
 
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </Provider>
 );

88079c757b37fdff88f6f418e6ac06cf9c761a39
Ro Sharp <ro.moreko@gmail.com>
2022-04-06 11:17:08 +1000

    Loader Component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,6 +3,7 @@ import React, { Fragment, useEffect } from "react";
 import MetaData from "./layout/MetaData";
 
 import Product from './product/Product';
+import Loader from './layout/Loader';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../actions/productActions';
@@ -20,7 +21,7 @@ export default function Home() {
   return (
     <Fragment>
 
-      {loading ? <h1>Loading...</h1> : (
+      {loading ? <Loader /> : (
         <Fragment>
           <MetaData title={`Buy Best Products Online`} />
           <h1 id="products_heading">Latest Products</h1>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 
@@ -15,28 +13,4 @@ root.render(
   <Provider store={store}>
     <App />
   </Provider>
-=======
-import ReactDOM from 'react-dom';
-=======
-import * as ReactDOMClient from 'react-dom/client';
->>>>>>> tmp
-import App from './App';
-
-import { Provider } from 'react-redux';
-import store from './store';
-
-const container = document.getElementById('root')
-
-const root = ReactDOMClient.createRoot(container);
-
-root.render(
-  <Provider store={store}>
-    <App />
-<<<<<<< HEAD
-  </React.StrictMode>,
-  document.getElementById('root')
->>>>>>> temp
-=======
-  </Provider>
->>>>>>> tmp
 );

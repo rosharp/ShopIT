8408546d7f0d99328380b460f80f2c1cd2a99119
Ro Sharp <ro.moreko@gmail.com>
2022-04-06 12:43:25 +1000

    Product Details Reducer & Action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,6 +4,9 @@ import {
   ALL_PRODUCTS_REQUEST,
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_FAIL,
+  PRODUCTS_DETAILS_REQUEST,
+  PRODUCTS_DETAILS_SUCCESS,
+  PRODUCTS_DETAILS_FAIL,
   CLEAR_ERRORS
 } from '../constants/productConstants';
 
@@ -20,6 +23,24 @@ export const getProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message
+    })
+  }
+}
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCTS_DETAILS_REQUEST })
+
+    const { data } = await axios.get(`/api/v1/product/${id}`)
+
+    dispatch({
+      type: PRODUCTS_DETAILS_SUCCESS,
+      payload: data.product
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCTS_DETAILS_FAIL,
       payload: error.response.data.message
     })
   }
--- frontend/src/reducers/productsReducer.js
+++ frontend/src/reducers/productsReducer.js
@@ -2,6 +2,9 @@ import {
   ALL_PRODUCTS_REQUEST,
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_FAIL,
+  PRODUCTS_DETAILS_REQUEST,
+  PRODUCTS_DETAILS_SUCCESS,
+  PRODUCTS_DETAILS_FAIL,
   CLEAR_ERRORS
 } from '../constants/productConstants';
 
@@ -34,5 +37,32 @@ export const productsReducer = (state = { products: [] }, action) => {
 
     default:
       return state;
+  }
+}
+
+export const productDetailsReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCTS_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case PRODUCTS_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload
+      }
+    case PRODUCTS_DETAILS_FAIL:
+      return {
+        ...state,
+        error: action.payload
+      }
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null
+      }
+    default:
+      return state
   }
 }

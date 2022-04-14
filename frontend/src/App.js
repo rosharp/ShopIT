f5cff528d0fc1a29aa8721abcc52ece2d6f72ba9
Ro Sharp <ro.moreko@gmail.com>
2022-04-14 14:53:48 +1000

    Load Logged In User

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/layout/Header';
@@ -9,7 +10,15 @@ import ProductDetails from './components/product/ProductDetails';
 import Login from './components/user/login';
 import Register from './components/user/Register';
 
+import { loadUser } from './actions/userActions';
+import store from './store';
+
 export default function App() {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
   return (
     <Router className="App">
       <Header />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -7,7 +7,9 @@ import {
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
   REGISTER_USER_FAIL,
-
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
   CLEAR_ERRORS
 } from '../constants/userConstants';
 
@@ -65,6 +67,36 @@ export const register = (userData) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: REGISTER_USER_FAIL,
+      payload: error.response.data.message
+    })
+  }
+}
+
+// Load user
+
+export const loadUser = () => async (dispatch) => {
+
+  try {
+    dispatch({
+      type: LOAD_USER_REQUEST
+    })
+
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }
+
+    const { data } = await axios.post('/api/v1/me')
+
+    dispatch({
+      type: LOAD_USER_SUCCESS,
+      payload: data.user
+    })
+
+  } catch (error) {
+    dispatch({
+      type: LOAD_USER_FAIL,
       payload: error.response.data.message
     })
   }

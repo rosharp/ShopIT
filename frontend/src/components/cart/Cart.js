51d1d4fa548f23c30598a0d56474981f783f79e5
Ro Sharp <ro.moreko@gmail.com>
2022-04-25 15:30:25 +1000

    Handle Quantity on Cart Component

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -12,6 +12,22 @@ export default function Cart() {
   const dispatch = useDispatch();
   const { cartItems } = useSelector(state => state.cart);
 
+  const increaseQty = (id, quantity, stock) => {
+    const newQty = quantity + 1;
+
+    if (newQty > stock) return;
+
+    dispatch(addItemToCart(id, newQty))
+  }
+
+  const decreaseQty = (id, quantity, stock) => {
+    const newQty = quantity - 1;
+
+    if (newQty <= 0) return;
+
+    dispatch(addItemToCart(id, newQty))
+  }
+
   return (
     <Fragment>
       <MetaData title={'Your Cart'} />
@@ -45,10 +61,10 @@ export default function Cart() {
 
                       <div className="col-4 col-lg-3 mt-4 mt-lg-0">
                         <div className="stockCounter d-inline">
-                          <span className="btn btn-danger minus">-</span>
-                          <input type="number" className="form-control count d-inline" value="1" readOnly />
+                          <span className="btn btn-danger minus" onClick={() => decreaseQty(item.product, item.quantity)}>-</span>
+                          <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
 
-                          <span className="btn btn-primary plus">+</span>
+                          <span className="btn btn-primary plus" onClick={() => increaseQty(item.product, item.quantity, item.stock)}>+</span>
                         </div>
                       </div>
 

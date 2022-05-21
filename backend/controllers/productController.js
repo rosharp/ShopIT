2225ea1c399d6cbe586c254d90d59f58f063578d
Ro Sharp <ro.moreko@gmail.com>
2022-05-21 11:00:04 +1000

    Delete Product Review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -219,28 +219,30 @@ exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
 })
 
 
-// Delete product Review => /api/v1/reviews
+// Delete Product Review   =>   /api/v1/reviews
 exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
-  const product = await Product.findById(req.query.prodictId);
+
+  const product = await Product.findById(req.query.productId);
+
+  console.log(product);
 
   const reviews = product.reviews.filter(review => review._id.toString() !== req.query.id.toString());
 
   const numOfReviews = reviews.length;
 
-  const ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / reviews.length;
+  const ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / reviews.length
 
   await Product.findByIdAndUpdate(req.query.productId, {
-    reviews,
-    ratings,
-    numOfReviews
+      reviews,
+      ratings,
+      numOfReviews
   }, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false
-  });
-
+      new: true,
+      runValidators: true,
+      useFindAndModify: false
+  })
 
   res.status(200).json({
-    success: true,
+      success: true
   })
 })

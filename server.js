de1d4dccc8d38dc2aab5ddf8cb81576122080d44
Ro Sharp <ro.moreko@gmail.com>
2022-05-27 09:36:40 +1000

    Fixed setting up config file

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 const connectDatabase = require('./config/database');
 
-const dotenv = require('dotenv');
 const cloudinary = require('cloudinary');
 
 // Handle Uncaught Exceptions
@@ -14,7 +13,7 @@ process.on('uncaughtException', err => {
 console.log(a);
 
 // Setting up config file
-dotenv.config({ path: 'backend/config/config.env' })
+if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
 // Connecting to database
 connectDatabase();
